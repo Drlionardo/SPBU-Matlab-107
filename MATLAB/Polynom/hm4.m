cefcfd8aa98b19a2931989984b9b6aac4ef097e6
Drlionardo <35406944+Drlionardo@users.noreply.github.com>
2020-06-08 10:51:56 +0300

    Poly hw added

--- MATLAB/Polynom/hm4.m
+++ MATLAB/Polynom/hm4.m
@@ -1,11 +1,24 @@
 n = input('n=');
+approx=0.0001; % погрешность для оценки корней
 syms x
 Poly_symb = x^(2*n) - n*x^(n+1)+n*x^(n-1) - 1;
 Poly= sym2poly(Poly_symb);
-PolyRoots=roots(Poly)
+PR=roots(Poly);% Корни полинома
 Derivative=polyder(Poly);
-DerivativeRoots=roots(Derivative)
-Lia = ismember(DerivativeRoots,PolyRoots)
+DR=roots(Derivative);% Корни производной
+DUB=[]; %Кратные корни
+for j =1 :length(PR)
+    for k=1:length(DR)
+        if(norm(PR(j)-DR(k),1)<approx) %Из-за погрешности вычисления не испольуется =
+            DUB=[DUB;PR(j)];
+            DR(k)=9999999;
+        end
+    end
+end
+disp("Кратные корни:")
+DUB=unique(DUB)
+a=real(min(PR))-1;
+b=real(max(PR))+1;
 
-
+fplot(Poly_symb,[a,b])
 
--- MATLAB/Polynom/hm5.m
+++ MATLAB/Polynom/hm5.m
@@ -1,26 +1,29 @@
 syms x
-
+%%
 disp('---First---');
-P1_1 = x.^2;
-P1_2  = (x-1).*(x+2).*(x+3);
-disp(make_deconved(P1_1, P1_2));
-
+Poly1 = x.^2;
+Poly2  = (x-1).*(x+2).*(x+3);
+disp(deconved(Poly1, Poly2));
+%%
 disp('---Second---');
-P2_1 = 3 + x;
-P2_2 = (x-1).*(x.^2+1);
-disp(make_deconved(P2_1, P2_2 ));
+Poly1 = 3 + x;
+Poly2 = (x-1).*(x.^2+1);
+disp(deconved(Poly1, Poly2 ));
 
-
+%%
 disp('----Third---');
-P3_1 = x.^2;
-P3_2 = (x.^4-1);
-disp(make_deconved(P3_1, P3_2 ));
+Poly1 = x.^2;
+Poly2 = (x.^4-1);
+disp(deconved(Poly1, Poly2 ));
 
-
-function [P, K] = make_deconved(P_1, P_2)
+%%Функци] разложения 
+function [P, K] = deconved(P1, P2)
     syms x
-    f_coeff = double(coeffs(P_1, 'all'));
-    s_coeff = double(coeffs(P_2, 'all'));
-    [r, s, K]=residue(f_coeff, s_coeff);
+    %Get all coeffs from P1 and P2
+    P1_coeff = double(coeffs(P1, 'all'));
+    P2_coeff = double(coeffs(P2, 'all'));
+    
+    [r, s, K]=residue(P1_coeff, P2_coeff);
+    %Build answer
     P = r./(x-s);
 end
