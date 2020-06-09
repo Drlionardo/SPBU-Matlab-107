2be65eedc0db8fbc55c4d70d8b60485309a456b7
Drlionardo <35406944+Drlionardo@users.noreply.github.com>
2020-06-09 19:21:58 +0300

    HW added

--- MATLAB/Roots1var/hm2.m
+++ MATLAB/Roots1var/hm2.m
@@ -1,3 +1,4 @@
+
 fun = @(x)x.^2+2.*x-1-sin(x); % function
 x0 = [-1 1]; % initial interval
 x = fzero(fun,x0)
--- MATLAB/Roots1var/hm5.m
+++ MATLAB/Roots1var/hm5.m
@@ -1,4 +1,3 @@
-
 syms x
 eqn = cos(x)-exp(0.001+x.^2)==0;
 fun = @(x) cos(x)-exp(0.001+x.^2);
