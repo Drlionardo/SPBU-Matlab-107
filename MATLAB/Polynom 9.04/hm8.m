377b5f294a378d0e560396d34609972aaa4f6dad
Drlionardo <35406944+Drlionardo@users.noreply.github.com>
2020-06-17 12:10:40 +0300

    Poly 8 fixed

--- MATLAB/Polynom 9.04/hm8.m
+++ MATLAB/Polynom 9.04/hm8.m
@@ -1,5 +1,15 @@
 n=25;
+
 M=rand(n,n);
 Roots=eig(M);
 P=poly(Roots);
 Pol2Txt(P)
+
+Values= polyval(P,M);
+Values=nnz(Values);
+
+if(Values==0)
+disp('Является корнем'); 
+else 
+disp('Не является корнем'); 
+end
