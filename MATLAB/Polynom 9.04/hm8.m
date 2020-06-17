f375f229fcddfae4d5e3495fdf39c6de8e7724df
Drlionardo <35406944+Drlionardo@users.noreply.github.com>
2020-06-17 12:51:44 +0300

    Update hm8.m

--- MATLAB/Polynom 9.04/hm8.m
+++ MATLAB/Polynom 9.04/hm8.m
@@ -5,19 +5,10 @@ P=poly(M);
 Pol2Txt(P)
 
 Roots_P=roots(P);
-Roots_P=sort(Roots_P);
-Roots_M=eig(M);
-Roots_M=sort(Roots_M);
+K=polyvalm(P,M);
 
-Bool=true;
-for k=1:n
-    if(Roots_M(k)-Roots_P(k)<0.000000000001)
-    else Bool=false;
-    end        
-end
-
-if(Bool)
-    disp('Является корнем'); 
-    else 
-    disp('Не является корнем'); 
+if(K(:)<0.00001)
+    disp("Yes")
+else
+    disp ("No");
 end
